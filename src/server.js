715ae0ff2c6e680c867f18f90e361b5a1ff0ec36
Sai_Chen <saikaungsanchen@gmail.com>
2026-01-24 22:01:52 -0500

    add eslint and prettier

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '172.25.111.33'; // bind to all interfaces so browsers outside WSL can reach it
 
 app.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`);
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
